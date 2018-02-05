4099438ce2f8fc8e4c01f14f1b9e491adc93accb
root <emil.emanuel@hotmail.com>
2018-02-05 16:34:30 +0100

    tested raspicam and flipped the pictures

--- models/raspicam.js
+++ models/raspicam.js
@@ -2,6 +2,6 @@
 
 var RaspiCam = require('raspicam')
 
-var camera = new RaspiCam({ mode: 'photo', output: '${/home/emil/Security-project}/test%d.jpg' })
-
+// vf = vertical flip - flipping the camera because it's upside down for some reason
+var camera = new RaspiCam({ mode: 'photo', output: '/home/emil/Security-project/test.jpg', vf: true, exposure: 'night', w: 1920, h: 1080 })
 camera.start()
