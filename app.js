4b2e896b3b9a838321b623466a3641abf38a5483
Emil <emil.emanuel@hotmail.com>
2018-02-05 11:24:53 +0100

    testing piCamera

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 'use strict'
 
-function hello () {
-  console.log('Hello World!')
-}
+const PiCamera = require('pi-camera');
+const myCamera = new PiCamera({
+  mode: 'photo',
+  output: '${/home/emil/test.jpg',
+  width: 640,
+  height: 480,
+  nopreview: true,
+})
 
-hello()
+myCamera.snap()
+  .then((result) => {
+    console.log('success!')
+  })
+  .catch((error) => {
+    console.log(error)
+  })
