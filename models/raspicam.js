ab7f26af5c94c788c8bbd0ef5690cf692f9091a0
Emil <emil.emanuel@hotmail.com>
2018-02-13 14:03:50 +0100

    removed test code

--- models/raspicam.js
+++ models/raspicam.js
@@ -2,15 +2,12 @@
 
 var RaspiCam = require('raspicam');
 var Sensor = require('pi-pir-sensor');
-//const tessel = require('tessel');
-const pir = require('pir').use(7);
 
 // vf = vertical flip - flipping the camera because it's upside down for some reason
 var camera;
-var tmp = 0;
 camera = new RaspiCam({
     mode: 'photo',
-    output: '/home/emil/Security-project/'+tmp+'test.jpg',
+    output: '/home/emil/Security-project/test.jpg',
     vf: true,
     // exposure: 'night',
     w: 1920,
@@ -18,25 +15,14 @@ camera = new RaspiCam({
 });
 //camera.start();
 
-// var sensor = new Sensor({
-//     pin: 7,
-//     loop: 1500
-// });
-//
-// sensor.on('movement', function() {
-//     camera.start();
-//     tmp = tmp + 1;
-// });
-//
-// //start sensor
-// sensor.start();
-
-pir.on('ready', pir => {
-  console.log('Ready...')
-  pir.on('movement:start', time => {
-    console.log(`Something moved! Time ${time}`)
-  });
-  pir.on('movement:end', time => {
-    console.log(`All is still. Time ${time}`);
-  });
+var sensor = new Sensor({
+    pin: 7,
+    loop: 1500
 });
+
+sensor.on('movement', function() {
+    camera.start();
+});
+
+//start sensor
+sensor.start();
