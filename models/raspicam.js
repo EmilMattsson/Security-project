9c37a903b49eca1f9960ac0ee7391ee2c162551a
Emil <emil.emanuel@hotmail.com>
2018-02-13 14:38:09 +0100

    refactor

--- models/raspicam.js
+++ models/raspicam.js
@@ -6,17 +6,23 @@ var Sensor = require('pi-pir-sensor');
 //const pir = require('pir').use(7);
 var gpio = require('rpi-gpio')
 var pir = { pin: 12, loopTime: 1500, tripped: false, value: undefined }
-var readInterval = function() { gpio.read(pir.pin, function(error, value) {
+
+var readInterval = function() {
+  gpio.read(pir.pin, function(error, value) {
     // we only want to move on if something changed
-     if (value === pir.tripped) {
-       return pir.tripped = value if (pir.tripped) console.log('tripped!')
-   }else {
-     console.log("it's quiet... a little TOO quiet...")
-   }
-  }) }
-  var onSetup = function(error) { if (error) console.error(error) return setInterval(readInterval, pir.loopTime) }
-  gpio.setMode(gpio.MODE_RPI)
-  gpio.setup(pir.pin, gpio.DIR_IN, onSetup)
+    if (value === pir.tripped) return pir.tripped = value
+    if (pir.tripped) console.log('tripped!')
+    else {
+      console.log("it's quiet... a little TOO quiet...")
+    }
+  })
+}
+
+var onSetup = function(error) {
+  if (error) console.error(error) return setInterval(readInterval, pir.loopTime)
+}
+gpio.setMode(gpio.MODE_RPI)
+gpio.setup(pir.pin, gpio.DIR_IN, onSetup)
 var camera;
 camera = new RaspiCam({
     mode: 'photo',
