3a424cad59e70c286304c018e93498bb4e0f8724
Emil <emil.emanuel@hotmail.com>
2018-02-13 11:02:44 +0100

    testing pir-sensor api

--- models/raspicam.js
+++ models/raspicam.js
@@ -1,14 +1,25 @@
-'use strict'
+'use strict';
 
-var RaspiCam = require('raspicam')
+var RaspiCam = require('raspicam');
+var Sensor = require('pi-pir-sensor');
 
 // vf = vertical flip - flipping the camera because it's upside down for some reason
-var camera = new RaspiCam({
-  mode: 'photo',
-  output: '/home/emil/Security-project/test.jpg',
-  vf: true,
-  exposure: 'night',
-  w: 1920,
-  h: 1080
-})
-camera.start()
+var camera;
+camera = new RaspiCam({
+    mode: 'photo',
+    output: '/home/emil/Security-project/test.jpg',
+    vf: true,
+    // exposure: 'night',
+    w: 1920,
+    h: 1080
+});
+//camera.start();
+
+var sensor = new Sensor({
+    pin: 12,
+    loop: 1500
+});
+
+sensor.on('movement', function() {
+    camera.start();
+});
