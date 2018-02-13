03a51989d2d46d867013db2e23cca47b08bfb445
KungenB <eb222sv@student.lnu.se>
2018-02-13 14:01:59 +0100

    testing

--- models/raspicam.js
+++ models/raspicam.js
@@ -3,9 +3,20 @@
 var RaspiCam = require('raspicam');
 var Sensor = require('pi-pir-sensor');
 //const tessel = require('tessel');
-const pir = require('pir').use(7);
-
-// vf = vertical flip - flipping the camera because it's upside down for some reason
+//const pir = require('pir').use(7);
+var gpio = require('rpi-gpio')
+var pir = { pin: 12, loopTime: 1500, tripped: false, value: undefined }
+var readInterval = function() { gpio.read(pir.pin, function(error, value) {
+    // we only want to move on if something changed
+     if (value === pir.tripped) {
+       return pir.tripped = value if (pir.tripped) console.log('tripped!')
+   }else {
+     console.log("it's quiet... a little TOO quiet...")
+   }
+  }) }
+  var onSetup = function(error) { if (error) console.error(error) return setInterval(readInterval, pir.loopTime) }
+  gpio.setMode(gpio.MODE_RPI)
+  gpio.setup(pir.pin, gpio.DIR_IN, onSetup)
 var camera;
 var tmp = 0;
 camera = new RaspiCam({
@@ -31,12 +42,12 @@ camera = new RaspiCam({
 // //start sensor
 // sensor.start();
 
-pir.on('ready', pir => {
-  console.log('Ready...')
-  pir.on('movement:start', time => {
-    console.log(`Something moved! Time ${time}`)
-  });
-  pir.on('movement:end', time => {
-    console.log(`All is still. Time ${time}`);
-  });
-});
+// pir.on('ready', pir => {
+//   console.log('Ready...')
+//   pir.on('movement:start', time => {
+//     console.log(`Something moved! Time ${time}`)
+//   });
+//   pir.on('movement:end', time => {
+//     console.log(`All is still. Time ${time}`);
+//   });
+// });
