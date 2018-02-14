2354f7968d1a7e35f9e324c2a9209a7e665fed39
Emil <emil.emanuel@hotmail.com>
2018-02-14 17:52:38 +0100

    Multi testing most recent

--- models/raspicam.js
+++ models/raspicam.js
@@ -21,6 +21,22 @@ let mailOptions = {
             {
                 filename: 'test0.jpg',
                 path: '/home/emil/Security-project/test0.jpg'
+            },
+            {
+                filename: 'test1.jpg',
+                path: '/home/emil/Security-project/test1.jpg'
+            },
+            {
+                filename: 'test2.jpg',
+                path: '/home/emil/Security-project/test2.jpg'
+            },
+            {
+                filename: 'test3.jpg',
+                path: '/home/emil/Security-project/test3.jpg'
+            },
+            {
+                filename: 'test4.jpg',
+                path: '/home/emil/Security-project/test4.jpg'
             }
         ]
 }
